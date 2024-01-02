d7a5e89c0dc30df292bcbd756f8b3e726eea9ed0
rmiller262988 <72883289+rmiller262988@users.noreply.github.com>
2024-01-02 16:10:42 -0500

    SimpleStorage

---
+++ ethers-simple-storage/deploy.js
@@ -0,0 +1,28 @@
+async function main() {
+    // compile them in our code
+    //or compile them separately. We will compile separately
+
+}
+
+
+
+
+
+    // synchronous [solidity]
+    // asynchronous [javascript]
+    //create pseudocode
+    //asynch await- javascript is asynch await
+
+    //function main() {   
+    //console.log("hi");
+    //let variable = 5;
+    //console.log(variable);
+   // }
+
+    main().then()) => process.exit(0)).catch((error)) => {
+        console.error(error);
+        process.exit(1);
+    
+    });
+
+
